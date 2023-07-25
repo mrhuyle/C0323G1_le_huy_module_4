8f05e6e2817f71f1e09212a8699b52df17b366dd
Le Huy <lehuy@Les-MacBook-Air.local>
2023-07-25 20:07:40 +0700

    ss6

--- ss6_spring_data_jpa/exercise/blog/src/main/java/com/example/blog/controller/BlogController.java
+++ ss6_spring_data_jpa/exercise/blog/src/main/java/com/example/blog/controller/BlogController.java
@@ -1,5 +1,6 @@
 package com.example.blog.controller;
 
+import com.example.blog.service.IAuthorService;
 import com.example.blog.service.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,13 @@ public class BlogController {
     @Autowired
     private IBlogService blogService;
 
+    @Autowired
+    private IAuthorService authorService;
+
     @GetMapping("/")
     public String showHomePage(Model model) {
         model.addAttribute("blogList", blogService.findAll());
+        model.addAttribute("author",authorService.findById(1));
         return "index";
     }
 }
---
+++ ss6_spring_data_jpa/exercise/blog/src/main/java/com/example/blog/service/IAuthorService.java
@@ -0,0 +1,9 @@
+package com.example.blog.service;
+
+import com.example.blog.model.Author;
+
+import java.util.List;
+
+public interface IAuthorService {
+    Author findById(Integer id);
+}
