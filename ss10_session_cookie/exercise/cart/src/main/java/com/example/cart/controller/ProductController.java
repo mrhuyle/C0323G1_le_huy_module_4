837417b910b6f733ecb55c545a941dabcac1a223
Le Huy <lehuy@Les-MacBook-Air.local>
2023-08-01 14:00:44 +0700

    ss10

--- ss10_session_cookie/exercise/cart/src/main/java/com/example/cart/controller/ProductController.java
+++ ss10_session_cookie/exercise/cart/src/main/java/com/example/cart/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.cart.model.Product;
 import com.example.cart.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,6 +22,11 @@ public class ProductController {
         return new Cart();
     }
 
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        model.addAttribute("productsList", productService.findAll());
+        return "home";
+    }
     @GetMapping("/shop")
     public ModelAndView showShop() {
         ModelAndView modelAndView = new ModelAndView("/shop");
@@ -30,17 +36,19 @@ public class ProductController {
 
     @GetMapping("/add/{id}")
     public String addToCart(@PathVariable Long id,
-                            @ModelAttribute Cart cart,
-                            @RequestParam("action") String action) {
+                            @ModelAttribute Cart cart) {
         Optional<Product> productOptional = productService.findById(id);
         if (!productOptional.isPresent()) {
             return "/error_404";
         }
-        if (action.equals("show")) {
-            cart.addProduct(productOptional.get());
-            return "redirect:/shopping-cart";
-        }
         cart.addProduct(productOptional.get());
         return "redirect:/shop";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        Product product = productService.findById(id).get();
+        model.addAttribute("product", product);
+        return "detail";
     }
 }
