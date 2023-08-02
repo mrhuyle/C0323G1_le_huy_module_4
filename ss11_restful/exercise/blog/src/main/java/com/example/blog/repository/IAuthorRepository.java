f5ce990a1de9c350a280ba3cc9d4a742040bdbc5
Le Huy <lehuy@Les-MacBook-Air.local>
2023-08-02 17:00:29 +0700

    abc

--- ss11_restful/exercise/blog/src/main/java/com/example/blog/repository/IAuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAuthorRepository extends JpaRepository<Author,Integer> {
-
-}
--- ss11_restful/exercise/blog/src/main/java/com/example/blog/service/IAuthorService.java
+++
@@ -1,9 +0,0 @@
-package com.example.blog.service;
-
-import com.example.blog.model.Author;
-
-import java.util.List;
-
-public interface IAuthorService {
-    Author findById(Integer id);
-}
